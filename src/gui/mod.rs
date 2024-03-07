29728e1cd0ac9a3d46a985ea06328a0f09e8df7d
tanguy-rdt <tanguy@roudaut.xyz>
2024-03-07 14:47:07 +0100

    gh-page

--- src/gui/mod.rs
+++
@@ -1,9 +0,0 @@
-pub mod gui;
-mod screen;
-mod menu;
-mod custom_widget;
-
-pub use gui::Gui;
-use screen::Screen;
-use menu::Menu;
-use menu::MenuSelection;
--- src/gui/screen/map/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod map;
-mod interactive_map;
-
-pub use map::Map;
-use interactive_map::InteractiveMap;
