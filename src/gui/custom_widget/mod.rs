cb494f8b3d1cebf1bc2ab426fe986958ced48e9e
tanguy-rdt <tanguy@roudaut.xyz>
2024-03-07 14:51:19 +0100

    gh-page-src

--- src/gui/custom_widget/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod toggle;
-pub mod circle_slider;
-pub mod compass;
-pub mod slider;
-
-pub use circle_slider::CircleSlider;
-pub use compass::Compass;
--- src/msgq/mod.rs
+++
@@ -1,2 +0,0 @@
-pub mod msgq;
-pub use msgq::MsgQ;
